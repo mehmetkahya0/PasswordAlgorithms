7ede332276962190c1e605ed587cd0914b7c844f
Mehmet Kahya <84154488+mehmetkahya0@users.noreply.github.com>
2023-08-14 10:36:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 # Password Generator
 # Mehmet Kahya - Start: 11.08.2023 
 
-
-
-# TODO: SOLVE LENGHT PROBLEM
 import random
 from colorama import Fore
 import time
+
 print(Fore.GREEN + f'''
             |////////////////////////////////////|      
             |/// PASSWORD GENERATOR VERSION 2 ///|
@@ -18,6 +16,7 @@ print(Fore.GREEN + f'''
 passwordList = ['']
 EncodeType = str(input(Fore.YELLOW + "Do you want spacial characters of your password? (~æ¨^~≈∂€ƒ),  (y/n)?  " + Fore.WHITE))
 
+
 if EncodeType=="y":
     characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-+*/=!&/()>£#$½§{[]}ğüşiöçĞÜŞİÖÇ@∑€®₺¥üiöπ¨~`æ´¬¨∆^ğƒ∂ßæ≈∫~µ≤≥÷|><|"
 
@@ -27,7 +26,6 @@ if EncodeType=="n":
 
 lenght = int(input(Fore.RED + "Set the length of the password: "))
 alternarive = int(input("How many passwords to generate: " + Fore.WHITE))
-
 
 for i in range(alternarive):
     password = "".join(random.choice(characters) for _ in range(lenght))
