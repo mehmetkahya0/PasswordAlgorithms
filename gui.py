f5bd4a2a640fa4e940eb0f398c183f3a5512ff73
Mehmet Kahya <84154488+mehmetkahya0@users.noreply.github.com>
2023-11-22 23:36:58 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
 #password algorithm gui app (main.py + gui.py)
-# 11/22/2023
 
 import time
 import random
@@ -15,10 +14,10 @@ root.title("Password Algorithms")
 root.geometry("500x500")
 root.resizable(False, False)
 p1 = PhotoImage(file = 'key.png') 
-  
+background = "#155b82"
 # Setting icon of master window 
 root.iconphoto(False, p1) 
-root.config(bg="#186894")
+root.config(bg=background)
 
 style = Style(root)
 style.theme_use("clam")
@@ -46,7 +45,7 @@ def back_to_black():
 
 def password_generator():
     
-    back_button = Button(root, text="<", font=("Helvetica", 15), bg="#186894", fg="red", command=back_to_black)
+    back_button = Button(root, text="<", font=("Helvetica", 15), bg=background, fg="red", command=back_to_black)
     back_button.place(x=10, y=27)
 
 
@@ -55,29 +54,29 @@ def password_generator():
     password_checker_button.destroy()
     ascii_art.destroy()
 
-    header = Label(root, text="Password Generator", font=("Helvetica", 30, "bold"), bg="#186894", fg="white", cursor="heart")
+    header = Label(root, text="Password Generator", font=("Helvetica", 30, "bold"), bg="background, fg="white", cursor="heart")
     header.place(x=90, y=20)
     
-    length = Label(root, text="Set the length of the password: ", font=("Helvetica", 15), bg="#186894", fg="white")
+    length = Label(root, text="Set the length of the password: ", font=("Helvetica", 15), bg=background, fg="white")
     length.place(x=10, y=100)
 
     length_entry = Entry(root, width=25, borderwidth=2)
     length_entry.place(x=235, y=100)
 
-    alternative = Label(root, text="How many passwords to generate: ", font=("Helvetica", 13), bg="#186894", fg="white")
+    alternative = Label(root, text="How many passwords to generate: ", font=("Helvetica", 13), bg=background, fg="white")
     alternative.place(x=10, y=150)
 
     alternative_entry = Entry(root, width=25, borderwidth=2)
     alternative_entry.place(x=235, y=150)
 
-    specialCharacters = Label(root, text="Do you want special characters in your password?", font=("Helvetica", 13), bg="#186894", fg="white")
+    specialCharacters = Label(root, text="Do you want special characters in your password?", font=("Helvetica", 13), bg=background, fg="white")
     specialCharacters.place(x=10, y=200)
 
     specialCharacters_check = IntVar()
     checkbutton = ttk.Checkbutton(root, variable=specialCharacters_check, onvalue=1, offvalue=0)
     checkbutton.place(x=330, y=200)
 
-    password_label = Label(root, text="", font=("Helvetica", 13), bg="#186894", fg="white")
+    password_label = Label(root, text="", font=("Helvetica", 13), bg=background, fg="white")
     password_label.place(x=30, y=300)
 
     
@@ -110,29 +109,30 @@ def password_generator():
         add_password()  # Start adding passwords
 
 
-    generate_button = Button(root, text="Generate", font=("Helvetica", 15, "bold"), bg="#186894", fg="red", command=generate)
+    generate_button = Button(root, text="Generate", font=("Helvetica", 15, "bold"), bg=background, fg="red", command=generate)
     generate_button.place(x=10, y=250)
 
 def password_checker():
-    back_button = Button(root, text="<", font=("Helvetica", 15), bg="#186894", fg="red", command=back_to_black, width=2)
+    back_button = Button(root, text="<", font=("Helvetica", 15), bg=background, fg="red", command=back_to_black, width=2)
     back_button.place(x=10, y=27)
 
     password_generator_button.destroy()
     password_checker_button.destroy()
+
     ascii_art.destroy()
     Mainheader.destroy()
 
-    header = Label(root, text="Password Checker", font=("Helvetica", 30, "bold"), bg="#186894", fg="white")
+    header = Label(root, text="Password Checker", font=("Helvetica", 30, "bold"), bg=background, fg="white")
     header.place(x=90, y=20)
 
 
-    password_label = Label(root, text="Enter your password: ", font=("Helvetica", 20), bg="#186894", fg="white")
+    password_label = Label(root, text="Enter your password: ", font=("Helvetica", 20), bg=background, fg="white")
     password_label.place(x=10, y=100)
 
     password_entry = Entry(root, width=30, borderwidth=2)
     password_entry.place(x=215, y=100)
 
-    result_label = Label(root, text="", font=("Helvetica", 13), bg="#186894", fg="white")
+    result_label = Label(root, text="", font=("Helvetica", 13), bg=background, fg="white")
     result_label.place(x=10, y=300)
 
     def check_password():
@@ -197,19 +197,19 @@ def password_checker():
 
         
 
-    check_button = Button(root, text="Check", font=("Helvetica", 15), bg="#186894", fg="black", command=check_password, width=30)
+    check_button = Button(root, text="Check", font=("Helvetica", 15), bg=background, fg="black", command=check_password, width=30)
     check_button.place(x=100, y=150)
 
 
 def mainScreen():
     global password_generator_button, password_checker_button, ascii_art, Mainheader
-    Mainheader = Label(root, text="Password Algorithms", font=("Helvetica", 30, "bold"), bg="#186894", fg="white")
+    Mainheader = Label(root, text="Password Algorithms", font=("Helvetica", 30, "bold"), bg=background, fg="white")
     Mainheader.place(x=90, y=20)
 
-    password_generator_button = Button(root, text="Password Generator", font=("Helvetica", 15, "bold"), bg="#186894", fg="black", command=password_generator, width=20, height=10)
+    password_generator_button = Button(root, text="Password Generator", font=("Helvetica", 15, "bold"), bg=background, fg="black", command=password_generator, width=20, height=10)
     password_generator_button.place(x=20, y=100)
 
-    password_checker_button = Button(root, text="Password Checker", font=("Helvetica", 15, "bold"),  bg='#186894',fg='black',command=password_checker, width=20, height=10)
+    password_checker_button = Button(root, text="Password Checker", font=("Helvetica", 15, "bold"),  bg=background,fg='black',command=password_checker, width=20, height=10)
     password_checker_button.place(x=260, y=100)
 
 
@@ -221,18 +221,18 @@ def mainScreen():
     ██╔═══╝ ██╔══██║╚════██║╚════██║ 
     ██║     ██║  ██║███████║███████║    
     ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝  
-''', font=("Helvatica", 10), bg="#186894", fg="white", cursor="heart red", justify="center", anchor="w")
+''', font=("Helvatica", 10), bg=background, fg="white", cursor="heart red", justify="center", anchor="w")
     ascii_art.place(x=75, y=300)
 
 
-    acopyright = Label(root, text="© 2023 - Password Algorithms - Mehmet Kahya", font=("Helvetica", 12, "bold"), bg="#186894", fg="white")
+    acopyright = Label(root, text="© 2023 - Password Algorithms - Mehmet Kahya", font=("Helvetica", 12, "bold"), bg=background, fg="white")
     acopyright.place(x=110, y=480)
 
 
 
 
 
-    credit = Button(root, text="Credits", font=("Helvetica", 10, "bold"), bg="#186894", fg="black", command=credits)
+    credit = Button(root, text="Credits", font=("Helvetica", 10, "bold"), bg=background, fg="black", command=credits)
     credit.place(x=440, y=475)
 mainScreen()
 
